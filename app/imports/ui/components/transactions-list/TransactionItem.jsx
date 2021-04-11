e4b6f95aab0914364337030a0edb537f5a95e6fa
Daphne Tapia <dmtapia@hawaii.edu>
2021-04-11 01:42:28 -1000

    fix eslint

--- app/imports/ui/components/transactions-list/TransactionItem.jsx
+++ app/imports/ui/components/transactions-list/TransactionItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Button, Icon, Modal, Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
